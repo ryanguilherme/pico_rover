7f855b1d08e1400a706df898bc5b30e3c86683ea
ryanguilherme <ryanguilhermetbt@gmail.com>
2024-09-11 23:05:41 -0300

    movement library first version done

--- rover/lib/movement/movement.c
+++ rover/lib/movement/movement.c
@@ -1,5 +1,42 @@
 #include "movement.h"
 
-void test() {
-	printf("test");
+void movement_init()
+{
+    gpio_init(IN1);     gpio_set_dir(IN1, GPIO_OUT);
+    gpio_init(IN2);     gpio_set_dir(IN2, GPIO_OUT);
+    gpio_init(IN3);     gpio_set_dir(IN3, GPIO_OUT);
+    gpio_init(IN4);     gpio_set_dir(IN4, GPIO_OUT);
 }
+
+void movement_forward()
+{
+    gpio_put(IN1, GPIO_HIGH);
+    gpio_put(IN2, GPIO_LOW);
+    gpio_put(IN3, GPIO_HIGH);
+    gpio_put(IN4, GPIO_LOW);
+}
+
+void movement_rotate_right()
+{
+    gpio_put(IN1, GPIO_HIGH);
+    gpio_put(IN2, GPIO_LOW);
+    gpio_put(IN3, GPIO_LOW);
+    gpio_put(IN4, GPIO_HIGH);
+}
+
+void movement_rotate_left()
+{
+    gpio_put(IN1, GPIO_LOW);
+    gpio_put(IN2, GPIO_HIGH);
+    gpio_put(IN3, GPIO_HIGH);
+    gpio_put(IN4, GPIO_LOW);
+}
+
+void movement_stop()
+{
+    gpio_put(IN1, GPIO_LOW);
+    gpio_put(IN2, GPIO_LOW);
+    gpio_put(IN3, GPIO_LOW);
+    gpio_put(IN4, GPIO_LOW);
+}
+
--- rover/lib/movement/movement.h
+++ rover/lib/movement/movement.h
@@ -2,12 +2,52 @@
 #define MOVEMENT_H
 #include "pico/stdlib.h"
 
+#define GPIO_HIGH 1
+#define GPIO_LOW  0
 
+// L298N INx PORTS
 #define IN1 5
 #define IN2 6
 #define IN3 7
 #define IN4 8
 
-void test();
+// L298N ATV PORT (duty cycle determines the motor speed)
+#define SPEED_PWM 9
+
+/*
+ * [NAME]:        Init
+ * [FUNCTION]:    movement_init()
+ * [PARAMETERS]:  void
+ * [DESCRIPTION]: Initialize every necessary pin for the L298N usage
+ */
+void movement_init();
+/*
+ * [NAME]:        Forward
+ * [FUNCTION]:    movement_forward()
+ * [PARAMETERS]:  void
+ * [DESCRIPTION]: Set the necessary combination of L298N INx ports to make the rover move forward
+ */
+void movement_forward();
+/*
+ * [NAME]:        Rotate Left
+ * [FUNCTION]:    movement_rotate_left()
+ * [PARAMETERS]:  void
+ * [DESCRIPTION]: Set the necessary combination of L298N INx ports to make the rover rotate left
+ */
+void movement_rotate_left();
+/*
+ * [NAME]:        Rotate Left
+ * [FUNCTION]:    movement_rotate_right()
+ * [PARAMETERS]:  void
+ * [DESCRIPTION]: Set the necessary combination of L298N INx ports to make the rover rotate right
+ */
+void movement_rotate_right();
+/*
+ * [NAME]:        Stop
+ * [FUNCTION]:    movement_stop()
+ * [PARAMETERS]:  void
+ * [DESCRIPTION]: Set the necessary combination of L298N INx ports to make the rover stop (stand still)
+ */
+void movement_stop();
 
 #endif //MOVEMENT_H
