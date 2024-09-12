c7ee57436ec99056562eeccfcf23ba73d51d05df
ryanguilherme <ryanguilhermetbt@gmail.com>
2024-09-12 16:07:45 -0300

    wandering function first concept

--- rover/lib/wandering/wandering.c
+++ rover/lib/wandering/wandering.c
@@ -2,5 +2,22 @@
 
 void wandering_setup()
 {
+    movement_init();
+    ultrasonic_init();
+}
 
+void wandering_loop()
+{
+
+    while(1)
+    {
+        while(ultrasonic_get_distance() <= 20)
+        {
+            movement_forward();
+        }
+        while(ultrasonic_get_distance() > 20)
+        {
+            movement_rotate_right();
+        }
+    }
 }
