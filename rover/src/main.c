763bb9abc89f2cae7c80f7f8e6e002144e67d3d5
ryanguilherme <ryanguilhermetbt@gmail.com>
2024-09-16 11:53:04 -0300

    removed tests from main

--- rover/src/main.c
+++ rover/src/main.c
@@ -13,42 +13,6 @@
 #include "wandering.h"
 #include "web.h"
 
-void movement_tests()
-{
-	movement_init();
-	sleep_ms(25);
-	while(1)
-	{
-		movement_forward();
-		sleep_ms(2500);
-		movement_stop();
-		sleep_ms(1000);
-		movement_rotate_left();
-		sleep_ms(2500);
-		movement_stop();
-		sleep_ms(1000);
-		movement_rotate_right();
-		sleep_ms(2500);
-	}
-}
-
-void ultrasonic_test()
-{
-	gpio_init(15);
-	gpio_set_dir(15, GPIO_OUT);
-	while(1)
-	{
-		double distance = ultrasonic_get_distance();
-		if (distance <= 10)
-		{
-			gpio_put(15, 0);
-		} else
-		{
-			gpio_put(15, 1);
-		}
-	}
-}
-
 int main()
 {
     web_setup();
