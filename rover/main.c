64892677963e7150b602d0fdf4f96e0b123cd726
ryanguilherme <ryanguilhermetbt@gmail.com>
2024-09-19 00:50:44 -0300

    added alert task and method implemented on other tasks

--- rover/main.c
+++ rover/main.c
@@ -9,15 +9,35 @@
 #include "dht11.h"
 #include "rain.h"
 
-#define QUEUE_LENGTH 10
+#define QUEUE_LENGTH    10
+#define ALERT_LED       13
 
 QueueHandle_t ultrasonic_queue;
+QueueHandle_t alert_queue;
 
 TaskHandle_t xDHT11TaskHandle;
 TaskHandle_t xUltrasonicTaskHandle;
 TaskHandle_t xWanderingTaskHandle;
+TaskHandle_t xAlertTaskHandle;
 
-void ultrasonic_task()
+void alert_task(void *pvParameters)
+{
+    printf("ALERT TASK INITIALIZED!\n");
+    uint queue_flag;
+    gpio_init(ALERT_LED);       gpio_set_dir(ALERT_LED, GPIO_OUT);
+    while(1)
+    {
+        xQueueReceive(alert_queue, &queue_flag, portMAX_DELAY/2);
+        if (queue_flag == 1)
+        {
+            gpio_put(ALERT_LED, GPIO_HIGH);
+            sleep_ms(250);
+            gpio_put(ALERT_LED, GPIO_LOW);
+        }
+    }
+}
+
+void ultrasonic_task(void *pvParameters)
 {
     ultrasonic_init();
     while(1)
@@ -26,18 +46,19 @@ void ultrasonic_task()
         if (xQueueSend(ultrasonic_queue, &distance, portMAX_DELAY) != pdPASS)
         {
             printf("[ERROR 0101]: Failed to send Ultrasonic Distance Data to respective queue!\n");
+            xQueueSend(alert_queue, 1, portMAX_DELAY/5);
         }
         vTaskDelay(pdMS_TO_TICKS(200));
     }
 }
 
-void wandering_task()
+void wandering_task(void *pvParameters)
 {
     wandering_setup();
     wandering_loop(ultrasonic_queue);
 }
 
-void dht11_task()
+void dht11_task(void *pvParameters)
 {
     int temperature;
     int humidity;
@@ -47,7 +68,10 @@ void dht11_task()
         if (dht11_read_data(&temperature, &humidity) == 0)
             printf("TEMPERATURE: %d, HUMIDITY: %d\n", temperature, humidity);
         else
+        {
             printf("[ERROR 0301]: Could not Read Data from DHT11\n");
+            xQueueSend(alert_queue, 1, portMAX_DELAY/5);
+        }
         vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
@@ -74,11 +98,12 @@ void rain_task(void *pvParameters)
         if (rain_read() == 1)
         {
             printf("[ALERT] IT IS CURRENTLY RAINING!\n");
+            xQueueSend(alert_queue, 1, portMAX_DELAY/5);
         }
     }
 }
 
-void pwm_test_task() {
+void pwm_test_task(void *pvParameters) {
     movement_init();
     movement_set_speed(47);
     while(1) {
@@ -98,10 +123,12 @@ int main() {
     //printf("WEB successfully setup\n");
 
     ultrasonic_queue = xQueueCreate(QUEUE_LENGTH, sizeof(double));
+    alert_queue = xQueueCreate(QUEUE_LENGTH, sizeof(uint));
     //
     xTaskCreate(dht11_task, "dht11Task", 256, NULL, 1, &xDHT11TaskHandle);
     xTaskCreate(wandering_task, "WanderingTask", 1024, NULL, 1, &xWanderingTaskHandle);
     xTaskCreate(ultrasonic_task, "UltrasonicTask", 1024, NULL, 1, &xUltrasonicTaskHandle);
+    xTaskCreate(alert_task, "AlertTask", 128, NULL, 0, &xAlertTaskHandle);
 
     //xTaskCreate(pwm_test_task, "pwmTestTask", 256, NULL, 1, NULL);
     //xTaskCreate(web_test_task, "webTestTask", 256, NULL, 1, NULL);
