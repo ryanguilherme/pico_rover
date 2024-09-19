f11e1061efea0e410e9c350a3627174a1691d6bc
guiaf04 <guilhermearaujo@alu.ufc.br>
2024-09-19 15:46:56 -0300

    remote control and scheduling done

--- rover/lib/remote_control/remote_control.c
+++ rover/lib/remote_control/remote_control.c
@@ -6,9 +6,17 @@ void remote_control_init()
     gpio_init(ROTATE_LEFT_PIN);     gpio_set_dir(ROTATE_LEFT_PIN, GPIO_IN);
     gpio_init(ROTATE_RIGHT_PIN);    gpio_set_dir(ROTATE_RIGHT_PIN, GPIO_IN);
     gpio_init(STOP_PIN);            gpio_set_dir(STOP_PIN, GPIO_IN);
+    // gpio_pull_up(MOVE_FORWARD_PIN);
+    // gpio_pull_up(ROTATE_LEFT_PIN);
+    // gpio_pull_up(ROTATE_RIGHT_PIN);
+    // gpio_pull_up(STOP_PIN);
 }
 
-bool remote_control_control()
+int remote_control_control()
 {
-    return (gpio_get(MOVE_FORWARD_PIN) == 1 && gpio_get(STOP_PIN) == 1) ? true : false;
+    if (gpio_get(MOVE_FORWARD_PIN) == 0 && gpio_get(STOP_PIN) == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
